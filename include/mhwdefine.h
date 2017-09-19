68eb0bab568b7cefa5e9c6ea76983e06e22221b0
mahongwei2016 <510492956@qq.com>
2017-09-19 11:04:15 +0800

    修改了mhwdefine.h并且写了示例程序

--- include/mhwdefine.h
+++ include/mhwdefine.h
@@ -1,8 +1,11 @@
 #ifndef __MHWDEFINE_H__
 #define __MHWDEFINE_H__
 #include <time.h>
+//1
 #define DIM(A) ((sizeof(A)/(sizeof(*A))))
+//2
 #define MALLOC(type,x) (type*)malloc(sizeof(type)*x)
+//3
 #define log(s) printf("%s:%d %s\n",__FILE__,__LINE__,s)
 #define LOG(s) do{		\
 	time_t mhw_t;		\
@@ -11,19 +14,22 @@
 	mhw_ti=localtime(&mhw_t);	\
 	printf("%s[%s:%d]:%s\n",asctime(mhw_ti),__FILE__,__LINE__,s);\
 	}while(0)
+//4
 #define mhw_buffer [100][1000];
 //typedef struct __sdb{}sdb;sdb *s=mhw_buffer;   //the start addr of mhw_buffer equal s
 //5
 #define CONVER(X) #X
 //6
-#define CALL(F,P) (printf("call function %s ",#F,f(p)))
+#define CALL(F,p) (printf("call function %s(%d):%d\n",#F,p,F(p)))
 //7
 #define STRUCT(type) typedef struct _mhw_##type type;struct _mhw_##type {char *name;int age;};
 //8
+#define printf_function
 #ifdef printf_function
 #define printf_function(); printf("%s begin\n",__FUNCTION__);
 #else
 #define printf_function(); 
 #endif
-
+//9
+#define printf_var(s) printf("%s:%d\n",#s,s)
 #endif
--- main.c
+++ main.c
@@ -2,6 +2,61 @@
 #include "mhwdefine.h"
 #include "softarray.h"
 #include "mhwlist.h"
+int f2c(unsigned char i)
+{
+	return (int)(i*i);
+}
+void define_test(void)
+{
+	//1
+	{
+		int buffer[100]={0};
+		printf("buffer length:%d\n",DIM(buffer));	
+	}
+	//2
+	{
+		char *buffer=MALLOC(int,100);
+		printf("buffer length:%d\n",DIM(buffer));	
+	}
+	//3
+	{
+		log("mhw");	
+	}
+	//4
+	{
+		LOG("mhw");	
+	}
+	{
+		int buffer[1000]={1,2,3,4};
+		typedef struct _sbd{
+			int a;
+			int b;
+			int c;
+		}sbd;
+		sbd* sbd_p=buffer;
+		printf_var(sbd_p->c);
+	}
+	//5
+	{
+		printf("%s\n",CONVER(mhw));	
+	}
+	//6
+	{
+		CALL(f2c,5);	
+	}
+	//7
+	STRUCT(student);
+	student mhw; 
+	mhw.name="mhw";
+	mhw.age=100;
+	printf("%s\t",mhw.name);
+	printf("%d\n",mhw.age);
+	//8
+	printf_function();
+	//9
+	int a=100;
+	printf_var(a);
+}
 int main(int argc,char**argv)
 {
     LOG("hello word");
@@ -34,9 +89,9 @@ int main(int argc,char**argv)
 			temp=list_entry(pos,struct student,l);
 			printf("num:%d\t,score%d\t",temp->num,temp->score);
 		}
-				
+		printf("\n");		
     }
-    
+    define_test();
     return 0;
 }
 
